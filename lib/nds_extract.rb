25fc0690052522203f9a882152c83186a705b5a8
Bri Turner <blturner3527@gmail.com>
2020-01-12 14:27:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,7 +14,7 @@ def directors_totals(nds)
     while new_index < nds[element_index][:movies].length do
       total = total + nds[element_index][:movies][new_index][:worldwide_gross]
     end
-    result << 
+    result << "#{nds[element_index][:name]"=>#{total},"
   end
 
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"

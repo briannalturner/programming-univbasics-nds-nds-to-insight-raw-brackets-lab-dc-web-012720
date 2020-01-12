865cb63251acb30db81668461cd7d4425ae3f92b
Bri Turner <blturner3527@gmail.com>
2020-01-12 14:27:55 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,6 +13,7 @@ def directors_totals(nds)
     #extract numbers
     while new_index < nds[element_index][:movies].length do
       total = total + nds[element_index][:movies][new_index][:worldwide_gross]
+      new_index += 1
     end
     result << "#{nds[element_index][:name]"=>#{total},"
     element_index += 1

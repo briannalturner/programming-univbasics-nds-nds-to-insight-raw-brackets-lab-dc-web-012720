3945cbf7516ef4ceed302378c5becafc89e3a48c
Bri Turner <blturner3527@gmail.com>
2020-01-12 14:32:25 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -17,24 +17,9 @@ def directors_totals(nds)
     result[nds[element_index][:name]] = total
     element_index += 1
   end
-
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
-    
-    
-    
-    
-    
+
   }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
   result
 end
 
